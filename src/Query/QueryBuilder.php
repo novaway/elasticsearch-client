f8697c815bfd8d3bf713dfbfc9f936731009d450
CheapHasz <devierne@gmail.com>
2017-07-28 16:35:11 +0200

    Remove forgotten var dump

--- src/Query/QueryBuilder.php
+++ src/Query/QueryBuilder.php
@@ -167,7 +167,6 @@ class QueryBuilder
         if (count($this->filterCollection)) {
             $this->queryBody['query']['filtered'] = $boolQuery;
             $this->queryBody['query']['filtered']['filter'] = $this->filterCollection;
-//            var_dump($this->queryBody);
         } else {
             $this->queryBody = array_merge($boolQuery, $this->queryBody);
         }
